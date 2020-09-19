f9916a25792ff2e580ec642f19befcd347fc1c84
Danil Ivanov <divanov@student.ethz.ch>
2020-09-19 18:28:55 +0200

    product model

--- meal_hack_flask/models/product.py
+++ meal_hack_flask/models/product.py
@@ -1,5 +1,6 @@
 from mongoengine import (
     Document, 
+    DynamicDocument, 
     StringField, 
     FloatField, 
     DateField, 
@@ -9,12 +10,33 @@ from mongoengine import (
     ListField
 )
 
-class Image(EmbeddedDocument):
-    original = StringField
-    stack = StringField 
+import re
 
-class Product(Document):
-    migrosId = IntField
-    name = StringField
-    description = StringField
+class Image(EmbeddedDocument):
+    original = StringField()
+    stack = StringField()
+
+class Product(DynamicDocument):
+    migros_id = IntField()
+    name = StringField()
+    description = StringField()
     image = EmbeddedDocumentField(Image)
+    allergen_text = StringField()
+    ingredient_id = IntField()
+
+def productFromMigros(migrosSource, ingredient_id) -> Product:
+    stripped_description = ""
+    if "description" in migrosSource:
+        stripped_description = re.sub('<[^<]+>', '', migrosSource["description"]["text"])
+
+    return Product(
+            migros_id = migrosSource["id"],
+            name = migrosSource["name"],
+            description = stripped_description,
+            image = {
+                'original': migrosSource["image"]["original"],
+                'stack': migrosSource["image"]["stack"],
+            },
+            allergen_text = migrosSource.get("allergen_text", "No allergens"),
+            ingredient_id = ingredient_id
+    )
