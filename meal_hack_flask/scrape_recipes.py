558fe8bfd323cca0ef41059867095f7de214d548
Danil Ivanov <divanov@student.ethz.ch>
2020-09-19 20:01:36 +0200

    added try/except block when saving recipes and products

--- meal_hack_flask/scrape_recipes.py
+++ meal_hack_flask/scrape_recipes.py
@@ -1,40 +1,57 @@
-from models.recepie import Recepie, recepieFromMigros
+from models.recipe import Recipe, recipeFromMigros
 from models.product import Product, productFromMigros
 import pip._vendor.requests as requests
 from pip._vendor.requests.auth import HTTPBasicAuth
 import json
 from pprint import pprint
 import mongoengine as me
+from mongoengine.errors import NotUniqueError
+
+
+def scrape_recipe(recipe_name:str):
+    queryUrl = f'https://hackzurich-api.migros.ch/hack/recipe/recipes_fr/_search?q={recipe_name}'
+    response = requests.get(queryUrl, auth=HTTPBasicAuth('hackzurich2020', 'uhSyJ08KexKn4ZFS'))
+    if response.status_code == 200:
+        data = response.json()
+        hits = data["hits"]["hits"]
+        print(f'{len(hits)} recipes found for {recipe_name}')
+        productSet = set()
+        for hit in hits : 
+            migrosSource = hit["_source"]
+            parsedRecipe = recipeFromMigros(migrosSource)
+            try:
+                parsedRecipe.save()
+                print(parsedRecipe.title)
+            except NotUniqueError:
+                pass
+
+            for ingredient in parsedRecipe.ingredients:
+                productSet.add(ingredient.migros_id)
+
+        print(f'{len(productSet)} possible products')
+        for ingredient_id in productSet:
+            queryUrl = f"https://hackzurich-api.migros.ch/products?recipe_ingredient={ingredient_id}"
+            response = requests.get(queryUrl, auth=HTTPBasicAuth('hackzurich2020', 'uhSyJ08KexKn4ZFS'))
+            if (response.status_code != 200): 
+                continue
+
+            data = response.json()
+            for product in data["products"]:
+                parsedProduct = productFromMigros(product, ingredient_id)
+                try:
+                    parsedProduct.save()
+                    print(parsedProduct.name)
+                except NotUniqueError:
+                    pass
+
+        print("Done")
 
 if __name__ == "__main__":
-
-    # Connect to database and reset the recepie collection
+    # Connect to database and reset the recipe collection
     me.connect('meal_hack')
-    Recepie.drop_collection()
-    
-    queryUrl = 'https://hackzurich-api.migros.ch/hack/recipe/recipes_fr/_search?q=pizza'
-    response = requests.get(queryUrl, auth=HTTPBasicAuth('hackzurich2020', 'uhSyJ08KexKn4ZFS'))
-    data = response.json()
+    # Recipe.drop_collection()
+    # Product.drop_collection()
 
-    hits = data["hits"]["hits"]
-    print(f'{len(hits)} recepies found')
-    productSet = set()
-    for hit in hits : 
-        print (hit["_source"]["title"])
-        migrosSource = hit["_source"]
-        parsedRecepie = recepieFromMigros(migrosSource)
-        # parsedRecepie.save()
-
-        # TODO scrape all the products as well
-        for ingredient in parsedRecepie.ingredients:
-            productSet.add(ingredient.migros_id)
-
-    print(productSet)
-    for ingredient_id in productSet:
-        queryUrl = f"https://hackzurich-api.migros.ch/products?recipe_ingredient={ingredient_id}"
-        response = requests.get(queryUrl, auth=HTTPBasicAuth('hackzurich2020', 'uhSyJ08KexKn4ZFS'))
-        data = response.json()
-        for product in data["products"]:
-            parsedProduct = productFromMigros(product, ingredient_id)
-            pprint(parsedProduct.to_json())
-            exit()
+    meals = ['sushi', 'chili con carne', 'tacos', 'curry', 'fondue', 'dahl', 'dim sum']
+    for meal in meals:
+        scrape_recipe(meal)
